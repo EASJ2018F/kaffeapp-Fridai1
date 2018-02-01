8a07ec6eea13c38b832b0f80c2aad26afd0d05f7
Fridai <Nikolaiviby3@gmail.com>
2018-02-01 09:36:44 +0100

    Done

---
+++ kaffe/FlatWhite.cs
@@ -0,0 +1,31 @@
+﻿namespace kaffe
+{
+    public class FlatWhite : Kaffe, Imælk
+    {
+        private int _pris;
+        private int _mIMælk;
+        private string _styrke;
+
+        public FlatWhite()
+        {
+            _pris = 45;
+            _mIMælk = 160;
+            _styrke = "mild";
+        }
+
+        public override int Pris()
+        {
+            return _pris;
+        }
+
+        public string Styrke()
+        {
+            return _styrke;
+        }
+
+        public int MlMælk()
+        {
+            return _mIMælk;
+        }
+    }
+}
---
+++ kaffeTests/FlatWhiteTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace kaffe.Tests
+{
+    [TestClass()]
+    public class FlatWhiteTest
+    {
+        [TestMethod()]
+        public void prisTest()
+        {
+            //Arrange
+            FlatWhite kaffe = new FlatWhite();
+            //Act
+            int pris = kaffe.Pris();
+            //Assert
+            Assert.AreEqual(45, pris);
+
+        }
+
+        [TestMethod()]
+        public void MiMælkTest()
+        {
+            //Arrange
+            FlatWhite kaffe = new FlatWhite();
+            //Act
+            int miMælk = kaffe.MlMælk();
+            //Assert
+            Assert.AreEqual(160, miMælk);
+
+        }
+
+        public void StykeTest()
+        {
+            //Arrange
+            FlatWhite kaffe = new FlatWhite();
+            //Act
+            string Styrke = kaffe.Styrke();
+            //Assert
+            Assert.AreEqual("mild", Styrke);
+
+        }
+    }
+}
